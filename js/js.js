6bbcf027b9f51e39ae5ea22812f18802ccdfb750
BenZ9413 <benedikt.sell@yahoo.de>
2022-01-29 11:33:53 +0100

    fix rematch and gameMode after AI game finished

--- js/js.js
+++ js/js.js
@@ -41,6 +41,7 @@ const Gameboard = (function(names) {
         possibleMoves = [0, 1, 2, 3, 4, 5, 6, 7, 8];
 
         turn = player1;
+        finished = false;
 
         const main = document.querySelector('.main');
 
